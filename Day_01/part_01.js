367421068be2d489059e5a025faf48607ebe7381
Alexandra Faller <alex@alexfaller.uk>
2023-12-02 11:16:10 +0000

    refactor part1

--- Day_01/part_01.js
+++ Day_01/part_01.js
@@ -1,34 +1,15 @@
 const fs = require("fs");
 
 const lines = fs
-  .readFileSync("Day_01/input.txt", { encoding: "utf-8" })
+  .readFileSync("Day_01/input_test.txt", { encoding: "utf-8" })
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trimEnd() // Remove ending whitespace
-   .split(/\n/)// puts them into an array
+  .split(/\n/); // puts them into an array
 
+var acc = 0;
 
-console.log(lines);
-
-const regex = /[0-9]/g
-var acc = 0
-
-
-
-
-const lineSplit = lines.map((line) => {
-const found = line.match(regex)
-
-const first = found[0]
-
-const second = found[found.length -1]
-
-const newNum = first + second
- 
-acc = acc + Number(newNum)
-
- console.log(acc)
- 
-  
-
-})
-
+lines.map((line) => {
+  const found = line.match(/[0-9]/g);
+  const newNum = found[0] + found[found.length - 1];
+  console.log((acc = acc + Number(newNum)));
+});
