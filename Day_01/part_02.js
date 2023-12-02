c4a5bcdb27c1b89a78d9a49060560a198324494b
Alexandra Faller <alex@alexfaller.uk>
2023-12-02 09:39:14 +0000

    Update part_02.js

--- Day_01/part_02.js
+++ Day_01/part_02.js
@@ -4,50 +4,40 @@ const lines = fs
   .readFileSync("Day_01/input_test2.txt", { encoding: "utf-8" })
   .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
   .trimEnd() // Remove ending whitespace
-   .split(/\n/)// puts them into an array
-
+  .split(/\n/); // puts them into an array
 
 console.log(lines);
+const regex = /[0-9]/g;
+var acc = 0;
+const numberMap = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
 
+const replaceWordNumbers = (str) => {
+  Object.keys(numberMap).forEach((key) => {
+    str = str.replace(new RegExp(key, "g"), numberMap[key]);
+  });
+  return str;
+};
 
+lines.map((line) => {
+  const newWordLine = replaceWordNumbers(line);
+  const found = newWordLine.match(regex);
+  console.log(found);
+  const first = found[0];
+  const second = found[found.length - 1];
+  const newNum = first + second;
 
-const regex = /[0-9]/g
+  acc = acc + Number(newNum);
 
-var acc = 0
-
-const numbers = {
- 0: 'zero',
- 1:'one',
- 2: 'two',
- 3: 'three',
- 4: 'four',
- 5: 'five',
- 6: 'six',
- 7: 'seven',
- 8: 'eight',
- 9: 'nine'
- }
- 
- console.log(Object.values(numbers))
-
-const lineSplit = lines.map((line) => {
- const hasNum = line.includes(Object.values(numbers))
- console.log(hasNum)
-
-
-//const found = line.match(regex)
-//
-//const first = found[0]
-//
-//const second = found[found.length -1]
-//
-//const newNum = first + second
-// 
-//acc = acc + Number(newNum)
-//
-// console.log(acc)
- 
-  
-
-})
-
+  console.log(acc);
+});
