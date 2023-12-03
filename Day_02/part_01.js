b0af0c80480ad395f8167eee8efadc0632354ea6
Alexandra Faller <alexandra.faller@itv.com>
2023-12-03 13:46:53 +0000

    complete part 1

--- Day_02/part_01.js
+++ Day_02/part_01.js
@@ -1,9 +1,48 @@
 const fs = require("fs");
 
 const lines = fs
-  .readFileSync("Day_02/input_test.txt", { encoding: "utf-8" })
-  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
-  .trimEnd() // Remove ending whitespace
-  .split(/\n/); // puts them into an array
+    .readFileSync("Day_02/input.txt", {encoding: "utf-8"})
+    .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+    .trimEnd() // Remove ending whitespace
+    .split(/\n/); // puts them into an array
 
 console.log(lines)
+const baseData = {
+    red: 12,
+    green: 13,
+    blue: 14
+}
+
+const extractColorCount = (round, color) => {
+    const regex = new RegExp(`(\\d+)\\s${color}`, 'i');
+    const match = round.match(regex);
+    return match ? parseInt(match[1], 10) : 0;
+};
+let acc = 0
+const games = lines.map((line, index) => {
+    const gameNumber = index + 1;
+
+    const rounds = line.split(';');
+    let gamePlayable = true;
+    // Map each round to an object
+    const roundData = rounds.map(round => {
+        const red = extractColorCount(round, 'red');
+        const green = extractColorCount(round, 'green');
+        const blue = extractColorCount(round, 'blue');
+
+        if (red <= baseData.red && blue <= baseData.blue && green <= baseData.green && gamePlayable === true) {
+            return
+        } else {
+            gamePlayable = false
+        }
+
+    });
+    if (gamePlayable === true) {
+         acc = acc + gameNumber
+        console.log(acc)
+    }
+
+});
+
+
+
