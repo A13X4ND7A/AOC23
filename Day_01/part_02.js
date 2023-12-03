22fd94b9cd1d4811ac00644396337ba77d0d237f
Alexandra Faller <alexandra.faller@itv.com>
2023-12-03 09:22:36 +0000

    solution wip test works not main

--- Day_01/part_02.js
+++ Day_01/part_02.js
@@ -1,54 +1,59 @@
 const fs = require("fs");
 
 const lines = fs
-  .readFileSync("Day_01/input_test2.txt", { encoding: "utf-8" })
-  .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
-  .trimEnd() // Remove ending whitespace
-  .split(/\n/); // puts them into an array
+    .readFileSync("Day_01/input.txt", {encoding: "utf-8"})
+    .replace(/\r/g, "") // remove all \r characters to avoid issues on Windows
+    .trimEnd() // Remove ending whitespace
+    .split(/\n/); // puts them into an array
 
-var acc = 0;
+let acc = 0;
 const numberMap = {
-  zero: '0',
-  one: '1',
-  two: '2',
-  three: '3',
-  four: '4',
-  five: '5',
-  six: '6',
-  seven: '7',
-  eight: '8',
-  nine: '9',
+    // zero: '0',
+    one: '1',
+    two: '2',
+    three: '3',
+    four: '4',
+    five: '5',
+    six: '6',
+    seven: '7',
+    eight: '8',
+    nine: '9',
 };
 
 console.log(lines);
 
- const findMatch = (str) => {
-   let strArr = [];
-   let keyIndexPairs = [];
-   let regex = /[0-9]+/g;
-   let match;
-   while ((match = regex.exec(str)) !== null) {
-     keyIndexPairs.push({ key: match[0], index: match.index });
-   }
-  Object.keys(numberMap).forEach((key) => {
-    let index = str.indexOf(key);
-    if (str.includes(key)){
-      if (index !== -1) {
-        let value = numberMap[key]; // Accessing the value from numberMap
-        keyIndexPairs.push({ value, index });
-        // keyIndexPairs.push({ key, index });
-      }
-      keyIndexPairs.sort((a, b) => a.index - b.index);
-      strArr = keyIndexPairs.map(pair => pair.value);
+const findMatch = (str) => {
+    let strArr = [];
+    let keyIndexPairs = [];
+    let regex = /[0-9]/g; //looks for 0-9
+    let match;
+    while ((match = regex.exec(str)) !== null) {
+        keyIndexPairs.push({ value: match[0], index: match.index });
+    }
+    Object.keys(numberMap).forEach((key) => {
+        let index = str.indexOf(key);
+            if (index !== -1) {
+                let value = numberMap[key];
+                keyIndexPairs.push({value, index}); // push the value and the index into the array
+            }
+    });
+    // Sort the 'keyIndexPairs' array in ascending order based on the 'index' property of each element.
+    keyIndexPairs.sort((a, b) => a.index - b.index);
+    // Transform each element in 'keyIndexPairs' to a number based on its 'value' property and set this to the 'strArr' array.
+    strArr = keyIndexPairs.map(pair => Number(pair.value));
+console.log(strArr)
+    const first = strArr[0]
+    const last = strArr[strArr.length-1]
+    const concatenated = Number(`${first}${last}`);
+    console.log({first}, {last}, {concatenated})
 
-      }
-  });
-   return strArr
- };
+    acc = acc + concatenated
+    console.log({acc}, '')
+    return acc
+};
+
 
 
 lines.map((line) => {
-  console.log(findMatch(line));
-  // console.log(newWordLine)
-  // console.log(acc = acc + Number(newWordLine));
+   findMatch(line)
 });
